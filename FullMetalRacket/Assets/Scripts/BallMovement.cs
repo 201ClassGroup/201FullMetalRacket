e2ea86cd1e83962aa877b1616894f977b9ad2661
Nate Mendoza <nmendoza82rg@aol.com>
2019-10-02 12:45:24 -0700

    Ball is moving good
    ball bounces on all edges nad all and trying to get the player movement feeling good

--- FullMetalRacket/Assets/Scripts/BallMovement.cs
+++ FullMetalRacket/Assets/Scripts/BallMovement.cs
@@ -14,10 +14,29 @@ public class BallMovement : AutoMove
         {
             ReversalMovement();
         }
+        else if (collidedObject.CompareTag("Wall"))
+        {
+            BounceOffWallMovement();
+        }
+        else if (collidedObject.CompareTag("BackWall"))
+        {
+            ReversalMovement();
+        }
 
     }
     public void ReversalMovement()
     {
+        float randomTurn = Random.Range(-.5f, .5f);
+
+        direction.x = randomTurn;
         direction.y *= -1;
     }
-}
+
+
+    public void BounceOffWallMovement()
+    {
+        direction.x *= -1;
+
+    }
+
+}// end of class
